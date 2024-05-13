84a8f09b1e490378fa21189814edb3aefccff5c1
victorlopescc <victorlopes.cc@gmail.com>
2024-05-13 11:48:11 -0300

    feat: Add empty list component for displaying tasks when the list is empty

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,35 @@ import { PlusCircle } from '@phosphor-icons/react';
 import { Header } from './components/Header';
 import { Input } from './components/Input';
 import { ButtonCreate } from './components/ButtonCreate';
+import { EmptyList } from './components/EmptyList';
 
 import styles from './App.module.css'
 
 export function App() {
+  const tasks = []
+
   return (
     <>
       <Header />
 
       <main className={styles.content}>
-        <section className={styles.search}>
+        <form className={styles.search}>
           <Input />
           <ButtonCreate>
             Criar
             <PlusCircle size={16} color="#f2f2f2" weight='bold' />
           </ButtonCreate>
+        </form>
+        <section className={styles.list}>
+          {
+            tasks.length === 0
+              ? (
+                <EmptyList />
+              )
+              : (
+                <p>{tasks.length}</p>
+              )
+          }
         </section>
 
       </main>
