59c137764da4d10f37a147e3cf906a821b6badd2
victorlopescc <victorlopes.cc@gmail.com>
2024-05-12 22:43:22 -0300

    refactor: Update header class name for consistency

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import styles from './Header.module.css';
 
 export function Header() {
     return (
-        <header className={styles.content}>
+        <header className={styles.header}>
             <img src="/logo.svg" />
         </header>
     )
